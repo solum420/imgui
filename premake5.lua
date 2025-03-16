ad93810ca0c35a98b511bb6cd39ecb32ff29b7f0
s0lum <s0lum@prop.surf>
2025-03-16 14:36:56 -0400

    aaaaaaaa

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "off"
+    warnings "off"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui_demo.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++20"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "Speed"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "Speed"
+        symbols "Off"
